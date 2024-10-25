aede1bb0f727988863e31210647e080227140660
ivanbot13 <ikduong100@student.hbuhsd.edu>
2024-10-25 10:40:31 -0700

    save

--- Main.java
+++ Main.java
@@ -4,6 +4,25 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter length of rectangle 1: ");
+    double length1 = sc.nextDouble();
+    System.out.println("Enter length of rectangle 2: ");
+    double length2 = sc.nextDouble();
+    System.out.println("Enter width of rectangle 1: ");
+    double width1 = sc.nextDouble();
+    System.out.println("Enter width of rectangle 2: ");
+    double width2 = sc.nextDouble();
+
+    Rectangle rec1 = new Rectangle(length1, width1);
+    Rectangle rec2 = new Rectangle(length2, width2);
+
+    boolean isRotated = (rec1.getLength() == rec2.getWidth() && rec1.getWidth() == rec2.getLength());
+    boolean isCongruent = isRotated || rec1.equals(rec2);
+    boolean isSimilar = isCongruent || ((rec1.getLength()/rec2.getLength()) == (rec1.getWidth()/rec2.getWidth()));
+
+    System.out.println(isRotated);
+    System.out.println(isCongruent);
+    System.out.println(isSimilar);
   }
 }
